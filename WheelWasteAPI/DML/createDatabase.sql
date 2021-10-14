b72e638e3a953edb0a79a9914d9a812b0e89fd61
Henrik Motzkus <hemotzku@microsoft.com>
2021-10-14 18:25:01 +0200

    DML added

---
+++ WheelWasteAPI/DML/createDatabase.sql
@@ -0,0 +1,78 @@
+﻿USE [hackthon1]
+GO
+/****** Object:  Table [dbo].[Device]    Script Date: 14.10.2021 18:24:10 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Device](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[Serial] [nchar](10) NOT NULL,
+ CONSTRAINT [PK_Device] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Product]    Script Date: 14.10.2021 18:24:10 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Product](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](50) NOT NULL,
+	[Description] [nvarchar](50) NULL,
+ CONSTRAINT [PK_Product] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Wheel]    Script Date: 14.10.2021 18:24:10 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Wheel](
+	[ID] [int] NOT NULL,
+	[DeviceID] [int] NOT NULL,
+	[Location] [nvarchar](50) NOT NULL,
+ CONSTRAINT [PK_Wheel] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[WheelWasteCollectionData]    Script Date: 14.10.2021 18:24:11 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[WheelWasteCollectionData](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[Date] [date] NOT NULL,
+	[Count] [int] NOT NULL,
+	[DeviceID] [int] NOT NULL,
+	[ProductID] [int] NOT NULL,
+ CONSTRAINT [PK_WheelWasteCollectionData] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[Wheel]  WITH CHECK ADD  CONSTRAINT [FK_Wheel_Device] FOREIGN KEY([DeviceID])
+REFERENCES [dbo].[Device] ([ID])
+GO
+ALTER TABLE [dbo].[Wheel] CHECK CONSTRAINT [FK_Wheel_Device]
+GO
+ALTER TABLE [dbo].[WheelWasteCollectionData]  WITH CHECK ADD  CONSTRAINT [FK_WheelWasteCollectionData_Device] FOREIGN KEY([DeviceID])
+REFERENCES [dbo].[Device] ([ID])
+GO
+ALTER TABLE [dbo].[WheelWasteCollectionData] CHECK CONSTRAINT [FK_WheelWasteCollectionData_Device]
+GO
+ALTER TABLE [dbo].[WheelWasteCollectionData]  WITH CHECK ADD  CONSTRAINT [FK_WheelWasteCollectionData_Product] FOREIGN KEY([ProductID])
+REFERENCES [dbo].[Product] ([ID])
+GO
+ALTER TABLE [dbo].[WheelWasteCollectionData] CHECK CONSTRAINT [FK_WheelWasteCollectionData_Product]
+GO
